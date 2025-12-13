6c60848aed06d78e16a213a740f39c86782621d9
HugoFara <hugo.farajallah@protonmail.com>
2025-12-13 12:36:56 +0100

    feat(pso): adds new, better formatted animations (#4).

--- src/pylinkage/visualizer/__init__.py
+++ src/pylinkage/visualizer/__init__.py
@@ -4,6 +4,10 @@ Linkage visualization features.
 
 __all__ = [
     "COLOR_SWITCHER",
+    "animate_dashboard",
+    "animate_parallel_coordinates",
+    "dashboard_layout",
+    "parallel_coordinates_plot",
     "plot_kinematic_linkage",
     "plot_static_linkage",
     "show_linkage",
@@ -20,5 +24,9 @@ from .animated import (
     swarm_tiled_repr as swarm_tiled_repr,
 )
 from .core import COLOR_SWITCHER as COLOR_SWITCHER
+from .pso_plots import animate_dashboard as animate_dashboard
+from .pso_plots import animate_parallel_coordinates as animate_parallel_coordinates
+from .pso_plots import dashboard_layout as dashboard_layout
+from .pso_plots import parallel_coordinates_plot as parallel_coordinates_plot
 from .static import plot_static_linkage as plot_static_linkage
 
